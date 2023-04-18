039f1d3b8c77b08cc339abf5aa8b60e69ed36468
pedro.troccoli <pedro.troccoli@appertech.com.br>
2023-04-17 22:42:36 -0300

    xv6labs

--- user/primes.c
+++ user/primes.c
@@ -5,33 +5,34 @@
 void
 filter(int lpipe[])
 {
-  int rpipe[2]; //file descriptors for pipe
-  pipe(rpipe); //cria o pipe
+// recebe um pipe como entrada, lê os números desse pipe e armazena em um array os números primos
+  int rpipe[2];
+  pipe(rpipe);
 
-  int primes[50]; vetor para armazenamento dos números encontrados
-  int c = 0; contador
-  char buf[1]; 
+  int primes[50];
+  int cnt = 0;
+  char buf[1];
 
-  while ((read(lpipe[0], buf, sizeof(buf))) != 0) { // leitura do pipe
-    primes[c++] = buf[0]; // armazena e incrementa o número lido
+  while ((read(lpipe[0], buf, sizeof(buf))) != 0) {
+    primes[cnt++] = buf[0];
   }
   close(lpipe[0]);
 
-  if (c == 0) return;
-
-  int first = primes[0]; // armazena o primeiro primo encontrado
+  if (cnt == 0) return;
+  // o primeiro número encontrado é usado para filtrar os outros números do array
+  int first = primes[0];
   printf("prime %d\n", first);
 
-  for (int i = 1; i < c; i++) {
+  for (int i = 1; i < cnt; i++) {
     if (primes[i] % first != 0) {
       char p = primes[i];
-      write(rpipe[1], &p, 1);
+      write(rpipe[1], &p, 1); //os números que não são múltiplos de "first" são escritos em um novo pipe
     }
   }
   close(rpipe[1]);
 
   int pid = fork();
-  if (pid == 0) {
+  if (pid == 0) { // filtra os números do novo pipe
     //child
     filter(rpipe);
   }
@@ -42,17 +43,18 @@ main()
 {
   int lpipe[2];
 
-  pipe(lpipe);
+  pipe(lpipe); // cria um novo pipe
 
   for (int i = 2; i <= 35; i++) {
     char p = i;
-    write(lpipe[1], &p, 1);
+    write(lpipe[1], &p, 1); //escreve números de 2 a 35 no novo pipe
   }
   close(lpipe[1]);
 
-  filter(lpipe);
+  filter(lpipe); //chama a função filter com o novo pipe criado como entrada
 
   wait((int *) 0);
 
   exit(0);
 }
+
